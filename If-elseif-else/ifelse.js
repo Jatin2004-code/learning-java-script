c91bd042174b1292012e40524a3b05f520354b94
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-03 14:46:05 +0530

    Added If-elseif-else code

---
+++ If-elseif-else/ifelse.js
@@ -0,0 +1,53 @@
+let age1 = 18;
+
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
+
+
+
+
+
+
+
+// Driving Eligibility Check
+let age = 19;
+
+console.log("Aapki age hai:", age);
+
+if (age > 18) {
+  console.log("Aap drive kar sakte hain. ✅");
+}
+else if (age == 18) {
+  console.log("Aap abhi driving license ke liye apply kar sakte hain. 📝");
+}
+else {
+  console.log("Aap abhi drive nahi kar sakte. ❌");
+}
+
+
+
+// Check if a number is even or odd
+let num = 7;
+
+if (num % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
+}
+
+
+// Grade Evaluation
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 75) {
+  console.log("Grade B");
+} else if (marks >= 60) {
+  console.log("Grade C");
+} else {
+  console.log("Fail");
+}
