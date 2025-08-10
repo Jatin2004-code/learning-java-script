7814d7119d6b0050f529b627c2df14971c429485
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-10 11:06:26 +0530

    feat: Add DOM methods Code

---
+++ DOM methods/Allmethod.js
@@ -0,0 +1,5 @@
+getElementById("id")         // 1 element by ID
+getElementsByClassName("c")  // HTMLCollection (multiple)
+getElementsByTagName("li")   // HTMLCollection (multiple)
+querySelector(".class")      // Pehla match only
+querySelectorAll("li")       // NodeList (loopable with forEach)
